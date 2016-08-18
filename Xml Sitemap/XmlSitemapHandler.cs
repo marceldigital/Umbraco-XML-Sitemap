91b27fb43730f83fb178c967d3a2037723ec4e10
Alex Vilmur <alex@vilmurnet.com>
2016-08-18 17:39:14 -0500

    Added the sitemap handler to the project.

---
+++ Xml Sitemap/XmlSitemapHandler.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+using System.Xml;
+using System.Xml.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace MarcelDigital.Umbraco.XmlSitemap {
+    /// <summary>
+    ///     Generetes the Xml sitemap for the umbraco website
+    /// </summary>
+    public class XmlSitemapHandler : IHttpHandler {
+        /// <summary>
+        ///     Specifies whether the handler can be reused in the pool
+        ///     or a new one needs to be created each time
+        /// </summary>
+        public bool IsReusable => true;
+
+        /// <summary>
+        ///     List of document type aliases to exclude from the sitemap
+        /// </summary>
+        private static readonly string[] DocumentTypeBlacklist = {"eventYear"};
+
+        /// <summary>
+        ///     Unique key to put in the cache to identify the sitemap
+        /// </summary>
+        private static readonly string CacheKey = Guid.NewGuid().ToString();
+
+        /// <summary>
+        ///     Default method for the http request
+        /// </summary>
+        /// <param name="context"></param>
+        public void ProcessRequest(HttpContext context) {
+            UmbracoContext.EnsureContext(
+                new HttpContextWrapper(HttpContext.Current),
+                ApplicationContext.Current,
+                true);
+
+            var sitemap = GenerateSitemapXml(context);
+
+            InsertSitemapIntoResponse(context, sitemap);
+        }
+
+        /// <summary>
+        ///     Generates the sitemap and inserts it in the response
+        /// </summary>
+        /// <param name="context"></param>
+        private static XDocument GenerateSitemapXml(HttpContext context) {
+            var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+
+            var siteRoot = umbracoHelper.TypedContentAtRoot().First();
+            XDocument sitemap = null;
+
+            if (!IsSitemapInCache(context)) {
+                var rootNode = GenerateSitemapRoot();
+
+                sitemap = new XDocument {
+                    Declaration = new XDeclaration("1.0", "utf-8", "yes")
+                };
+
+                sitemap.Add(rootNode);
+
+                AddPagesToSitemap(rootNode, siteRoot);
+
+                PutSitemapInCache(context, sitemap);
+            } else {
+                sitemap = GetSitemapFromCache(context);
+            }
+
+            return sitemap;
+        }
+
+        /// <summary>
+        ///     Generates the root of the xml sitemap
+        /// </summary>
+        /// <returns></returns>
+        private static XElement GenerateSitemapRoot() {
+            XNamespace ns = "http://www.sitemaps.org/schemas/sitemap/0.9";
+            XNamespace xhtml = "http://www.w3.org/1999/xhtml";
+
+            var root = new XElement("urlset",
+                new XAttribute("xmlns", ns),
+                new XAttribute(XNamespace.Xmlns + "xhtml", xhtml));
+
+            return root;
+        }
+
+        /// <summary>
+        ///     Adds the umbraco pages to the sitemap which aren't in the blacklist and have
+        ///     a template
+        /// </summary>
+        private static void AddPagesToSitemap(XElement root, IPublishedContent contentRoot) {
+            foreach (
+                var content in
+                    contentRoot.Descendants()
+                               .Where(d => !DocumentTypeBlacklist.Contains(d.DocumentTypeAlias))
+                               .Where(d => d.TemplateId > 0)) {
+                root.Add(new XElement("url", new XElement("loc", content.UrlWithDomain()),
+                    new XElement("lastmod", content.UpdateDate.ToString("yyyy-MM-ddTHH:mm:sszzz")),
+                    new XElement("changefreq", "weekly"),
+                    new XElement("priority", "0.5")
+                    ));
+            }
+        }
+
+        /// <summary>
+        ///     Checks if the sitemap is in the cache
+        /// </summary>
+        /// <returns></returns>
+        private static bool IsSitemapInCache(HttpContext context)
+            => context.Cache[CacheKey] != null || context.Cache[CacheKey] is XDocument;
+
+        /// <summary>
+        ///     Gets the sitemap from the cache
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private static XDocument GetSitemapFromCache(HttpContext context)
+            => context.Cache[CacheKey] as XDocument;
+
+        /// <summary>
+        ///     Puts the sitemap in the cache for one day
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="sitemap"></param>
+        private static void PutSitemapInCache(HttpContext context, XDocument sitemap)
+            => context.Cache.Insert(CacheKey, sitemap, null, DateTime.Now.AddDays(1), Cache.NoSlidingExpiration);
+
+        /// <summary>
+        ///     Puts the sitemap into the http response
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="sitemap"></param>
+        private static void InsertSitemapIntoResponse(HttpContext context, XDocument sitemap) {
+            var response = context.Response;
+            response.Clear();
+            response.ContentType = "text/xml";
+
+            using (var streamWriter = new StreamWriter(response.OutputStream, Encoding.UTF8)) {
+                var xmlWriter = new XmlTextWriter(streamWriter);
+                sitemap.WriteTo(xmlWriter);
+            }
+            response.End();
+        }
+    }
+}
