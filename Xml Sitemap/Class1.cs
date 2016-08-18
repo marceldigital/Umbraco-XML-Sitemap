74856c3db1edb94e968e081e4d71b77fafc58c34
Alex Vilmur <alex@vilmurnet.com>
2016-08-18 17:33:22 -0500

    Add project files.

---
+++ Xml Sitemap/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xml_Sitemap
+{
+    public class Class1
+    {
+    }
+}
