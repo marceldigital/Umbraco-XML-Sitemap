2d1cefa69bfee95e3a070319a3cee0f102eb7100
Tim Geyssens <tim@nibble.be>
2018-10-18 16:10:22 +0200

    Adds domain initializer and hostname cache + updates readme

---
+++ Xml Sitemap/Initializers/DomainInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace MarcelDigital.UmbracoExtensions.XmlSitemap.Initializers
+{
+    public class DomainInitializer : BaseUmbracoInitializer, IInitializer
+    {
+        public DomainInitializer(UmbracoHelper helper) : base(helper) { }
+
+        public IEnumerable<IPublishedContent> GetContent()
+        {
+            var requestedHostname = Helper?.UmbracoContext?.HttpContext?.Request?.Url?.Host ?? "";
+            var umbracoDomain = Helper?.UmbracoContext?.Application?.Services?.DomainService?.GetByName(requestedHostname);
+
+            if (umbracoDomain == null) return new List<IPublishedContent>();
+
+            var content = Helper.TypedContent(umbracoDomain.RootContentId);
+
+            return content?.DescendantsOrSelf() ?? new List<IPublishedContent>();
+        }
+    }
+}
---
+++ Xml Sitemap/Optimization/HostnameCache.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+using System.Xml.Linq;
+
+namespace MarcelDigital.UmbracoExtensions.XmlSitemap.Optimization
+{
+    public class HostnameCache : ISitemapCache
+    {
+        /// <summary>
+        ///     Unique key to put in the cache to identify the sitemap
+        /// </summary>
+        private static readonly string BaseCacheKey = Guid.NewGuid().ToString();
+
+        private readonly string CacheKey;
+
+        /// <summary>
+        ///     The current http context
+        /// </summary>
+        private readonly HttpContext _httpContext;
+
+        /// <summary>
+        ///     Default constructor for the cache.
+        /// </summary>
+        public HostnameCache() : this(HttpContext.Current) { }
+
+        /// <summary>
+        ///     Constructor for the cache.
+        /// </summary>
+        /// <param name="httpContext">The current http context.</param>
+        public HostnameCache(HttpContext httpContext)
+        {
+            _httpContext = httpContext;
+            CacheKey = $"{BaseCacheKey}-{_httpContext?.Request?.Url?.Host ?? "unkown"}";
+        }
+
+        /// <summary>
+        ///     Checks if the sitemap is in the cache
+        /// </summary>
+        /// <returns></returns>
+        public bool IsInCache()
+            => (_httpContext.Cache[CacheKey] != null) || _httpContext.Cache[CacheKey] is XDocument;
+
+        /// <summary>
+        ///     Gets the sitemap from the cache
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public XDocument Retrieve()
+            => _httpContext.Cache[CacheKey] as XDocument;
+
+        /// <summary>
+        ///     Puts the sitemap in the cache for one day
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="sitemap"></param>
+        public void Insert(XDocument sitemap)
+            => _httpContext.Cache.Insert(CacheKey, sitemap, null, DateTime.Now.AddDays(1), Cache.NoSlidingExpiration);
+
+       
+
+       
+    }
+}
